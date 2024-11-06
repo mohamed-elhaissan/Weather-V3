6b2adfbd74c18d0eaf16ba533b5dcd4671bae16b
mohamed-elhaissan <mhalwasoffice@gmail.com>
2024-11-06 23:44:23 +0100

    updating

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,8 @@
+import Introduction from './Introduction.jsx'
 export default function Home(){
     return (
-        <div>
-            home
+        <div className="bg-red-400 h-[100vh] flex justify-center items-center">
+            <Introduction/>
         </div>
     )
 }
---
+++ src/Components/Introduction.jsx
@@ -0,0 +1,7 @@
+export default function Introduction(){
+    return (
+        <div className="">
+            <h1>Good Morning , Mohamed !</h1>
+        </div>
+    )
+}
