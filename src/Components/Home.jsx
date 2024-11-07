81b32eb34bbadec11a98779592243d0ca03b4dca
mohamed-elhaissan <mhalwasoffice@gmail.com>
2024-11-07 02:46:45 +0100

    updating

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import Introduction from './Introduction.jsx'
 export default function Home(){
     return (
-        <div className="bg-red-400 h-[100vh] flex justify-center items-center">
+        <div className="h-[95vh]  flex justify-center items-center">
             <Introduction/>
         </div>
     )
--- src/Components/Introduction.jsx
+++ src/Components/Introduction.jsx
@@ -1,7 +1,40 @@
-export default function Introduction(){
-    return (
-        <div className="">
-            <h1>Good Morning , Mohamed !</h1>
-        </div>
-    )
+import { AnimatePresence, delay, motion } from "framer-motion";
+export default function Introduction() {
+  return (
+    <div className="">
+      <h1 className="text-5xl">
+        <AnimatedHeader content={"Good Morning , Mohamed !"} />
+      </h1>
+      <p className="text-center">Weather at Your Fingertips</p>
+    </div>
+  );
 }
+const AnimatedHeader = ({ content }) => {
+  console.log(content.split(""));
+
+  return (
+    <AnimatePresence>
+      <div>
+        {content.split("").map((item, index) => (
+          <motion.span
+            key={index}
+            initial={{ opacity: 0, scale: 0, y: "100px" }}
+            animate={{ opacity: 1, scale: 1, y: "0px" }}
+            transition={{
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+              delay: 0.025 * index,
+
+              ease: "easeInOut",
+              duration: 0.25,
+            }}
+            className="inline-block"
+          >
+            {item}
+          </motion.span>
+        ))}
+      </div>
+    </AnimatePresence>
+  );
+};
