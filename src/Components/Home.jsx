53e09e839ba279d54840594d5c1a4aa5a2816d93
mohamed-elhaissan <mhalwasoffice@gmail.com>
2024-11-08 18:32:37 +0100

    updating

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Input } from "../Context/InputContext.jsx";
 import Introduction from "./Introduction.jsx";
 import { motion } from "framer-motion";
+import clouds from "../../public/clouds.svg"
 export default function Home() {
   const { inputValue } = useContext(Input);
   console.log(inputValue);
@@ -16,14 +17,55 @@ export default function Home() {
             initial={{ y: "40px", opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{duration : 0.5}}
-            className="text-3xl"
+            className="text-3xl text-center"
           >
             The Weather in {inputValue}
           </motion.h1>
-          <div>
+          <div className="bg-white rounded-xl px-5 items-center gap-9 shadow-custom-shadow flex justify-between mt-5">
+            <h2 className="text-2xl">22 <sub>C</sub></h2>
+            <div className="bg-[#F7F6F7] py-4 my-2 px-2 rounded-lg shadow-custom-shadow">
+              
+                  <h3 className="text-3xl">Cloudy</h3> 
+                  <p className="text-sm">Its Look a good weather To do Something</p>
+              
+            </div>
+          </div>
+          <div className="mt-10 bg-white flex text-center p-5 rounded-lg">
+            <div>
+                <h4>NOW</h4>
+                <img src={clouds} alt="" />
             <h2>22 <sub>C</sub></h2>
-                <h3>Cloudy</h3>
-            
+                
+                
+            </div>
+            <div>
+                <h4>NOW</h4>
+                <img src={clouds} alt="" />
+            <h2>22 <sub>C</sub></h2>
+                
+                
+            </div>
+            <div>
+                <h4>NOW</h4>
+                <img src={clouds} alt="" />
+            <h2>22 <sub>C</sub></h2>
+                
+                
+            </div>
+            <div>
+                <h4>NOW</h4>
+                <img src={clouds} alt="" />
+            <h2>22 <sub>C</sub></h2>
+                
+                
+            </div>
+            <div>
+                <h4>NOW</h4>
+                <img src={clouds} alt="" />
+            <h2>22 <sub>C</sub></h2>
+                
+                
+            </div>
           </div>
         </motion.div>
       )}
